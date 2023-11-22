1ba51e5083943bd0184c20a9c7ebe7debc9c6f2e
Gati krishna Meher <gatimeher@canoesolutions.com>
2023-11-22 15:42:54 +0530

    Inital Commit

---
+++ src/main/java/com/mkpits/railway/RailwaySystemJavaApr2023Application.java
@@ -0,0 +1,13 @@
+package com.mkpits.railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaySystemJavaApr2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaySystemJavaApr2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkpits/railway/RailwaySystemJavaApr2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkpits.railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaySystemJavaApr2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
