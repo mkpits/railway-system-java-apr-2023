0e84cb432c31808b1e80e239044edc33d3188d1d
abhisheksatale1 <abhisheksatale4@gmail.com>
2024-01-11 12:07:09 +0530

    railway reservation

--- src/main/java/com/mkpits/railway/controller/StationMasterController.java
+++ src/main/java/com/mkpits/railway/controller/StationMasterController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class StationMasterController {
     private StationMasterService stationMasterService;
 
+
     @Autowired
     public StationMasterController(StationMasterService stationMasterService) {
         this.stationMasterService = stationMasterService;
@@ -53,5 +54,6 @@ public class StationMasterController {
     return"stationInformation/StationMasterAddForm";
 
     }
+
 
 }
